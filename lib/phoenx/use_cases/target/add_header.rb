802af526b61568959eefb29ba9787c0fa6278f01
Simon Seyer <simon.seyer@gmail.com>
2017-02-23 11:21:10 +0200

    Implemented generation of umbrella headers

--- lib/phoenx/use_cases/target/add_header.rb
+++ lib/phoenx/use_cases/target/add_header.rb
@@ -9,12 +9,16 @@ module Phoenx
 			
 			attr_reader :target_spec
 
+			attr_reader :umbrella_headers
+
 			def initialize(project, target, target_spec)
 			
 				@project = project
 				@target = target
 
 				@target_spec = target_spec
+			
+				@umbrella_headers = []
 
 			end
 
@@ -24,27 +28,51 @@ module Phoenx
 				self.add_project_headers
 				self.add_private_headers
 
+				self.generate_umbrella_header
+
 			end
 			
 			def add_public_headers
 			
-				self.add_headers(@target_spec.public_headers, ATTRIBUTES_PUBLIC_HEADERS)
+				self.add_headers(@target_spec.public_headers, ATTRIBUTES_PUBLIC_HEADERS, true)
 		
 			end
 
 			def add_project_headers
 			
-				self.add_headers(@target_spec.project_headers, ATTRIBUTES_PROJECT_HEADERS)
+				self.add_headers(@target_spec.project_headers, ATTRIBUTES_PROJECT_HEADERS, true)
 		
 			end
 			
 			def add_private_headers
 			
-				self.add_headers(@target_spec.private_headers, ATTRIBUTES_PRIVATE_HEADERS)
+				self.add_headers(@target_spec.private_headers, ATTRIBUTES_PRIVATE_HEADERS, false)
 		
 			end
+			
 
-			def add_headers(header_files,attributes)
+			def generate_umbrella_header
+
+				unless @target_spec.umbrella_header == nil
+					
+					entries = @umbrella_headers.map{ |header| 
+						unless @target_spec.module_name == nil
+							import = '<' + @target_spec.module_name + '/' + File.basename(header) + '>'
+						else
+							import = '"'  + File.basename(header) + '"'
+						end
+						'#import ' + import
+					}
+
+					open(@target_spec.umbrella_header, "w") { |file| 
+						entries.each { |header| file.puts header }
+					}
+
+				end
+				
+			end
+
+			def add_headers(header_files, attributes, add_to_umbrella_header)
 			
 				headers = Phoenx.merge_files_array(header_files)
 
@@ -60,6 +88,10 @@ module Phoenx
 		
 					build_file = @target.headers_build_phase.add_file_reference(file, true)
 					build_file.settings = attributes
+
+					if add_to_umbrella_header
+						@umbrella_headers << header
+					end
 
 				end
 		
