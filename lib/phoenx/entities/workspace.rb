91befd39b11b892cd58a540d75a79cf4da4021fb
Simon Seyer <simon.seyer@gmail.com>
2017-05-25 23:36:08 +0200

    Added ability to integrate existing projects into workspace

--- lib/phoenx/entities/workspace.rb
+++ lib/phoenx/entities/workspace.rb
@@ -5,15 +5,20 @@ module Phoenx
 		attr_reader   :main_project_name
 		attr_reader   :main_project_path
 		attr_reader   :projects
+		attr_reader   :generated_projects
 		
 		def initialize
 			@main_project = {}
 			@projects = {}
+			@generated_projects = {}
 			yield(self)
 		end
 		
-		def project(name, path = nil)
+		def project(name, path = nil, generate = true)
 			@projects[name] = path
+			if generate
+				@generated_projects[name] = path
+			end
 		end
 		
 		def main_project(name, path = nil)
