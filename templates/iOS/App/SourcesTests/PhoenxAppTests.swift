60bd68035106f1c10c305c3b7007756d30cd68eb
Karsten Litsche <feedback@objectalchemist.com>
2018-03-25 18:24:26 +0200

    #44 Enhance iOS-App example with a unittest target

---
+++ templates/iOS/App/SourcesTests/PhoenxAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  PhoenxAppTests.swift
+//  PhoenxAppTests
+//
+//  Created by Karsten Litsche on 25.03.18.
+//
+
+import XCTest
+
+class PhoenxAppTests: XCTestCase {
+    
+    func testExample() {
+        XCTAssert(false)
+    }
+    
+}
